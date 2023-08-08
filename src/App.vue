ac53143ba6c249d96029331454ea1dec50c2a080
InawoVictor <inawovictor004@gmail.com>
2023-08-08 14:12:47 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <h2>Kenzo Reaction Timer</h2>
+  <button @click="start" :disabled="isPlaying" > Play</button>
+  <Block v-if="isPlaying" :delay="delay"  @end="endGame"/>
+  <Result  v-if="showResult" :score="score"/>
+  
+</template>
+
+<script>
+//v-if="showResult"
+import Block from "./components/Block"
+import Result from "./components/Result"
+
+export default {
+  name: 'App',
+  components: {
+   Block,
+   Result
+  },
+  data() {
+    return{
+      isPlaying: false,
+      showResult: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.showResult = false
+      this.delay = 2000 + Math.random() * 5000
+    },
+    endGame (reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background: white;
+}
+button{
+  color: white;
+  background: #0faf87;
+  border: none;
+  padding: 8px 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+  border-radius: 4px;
+}
+button[disabled]{
+  opacity: .2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="result">
+        <p>Reaction time - {{score}}ms</p>
+        <p class="rank">{{rank}}</p>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "Result",
+    props: ['score'],
+    data() {
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = " Ninja reflexes"
+        } else if (this.score < 350) {
+            this.rank = "Rapid reflexes"
+        } else {
+            this.rank = "Snail pace..."
+        }
+    },
+}
+</script>
+
+<style>
+    .result{
+        width: 400px;
+        border-radius: 20px;
+        background: teal;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+    .rank {
+        font-size: 1.4em;
+        font-weight: bold;
+    }
+</style>
